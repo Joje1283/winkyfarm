ad1af4e7a9bc45165e32f952eb1072df692363f5
Jaesik <joje1283@gmail.com>
2022-07-15 00:29:03 +0900

    * poetry에 dev환경으로 django-debug-toolbar 라이브러리 추가 * django-debug-toolbar 셋업 * 유저 테이블이 어드민에 출력되도록 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
+from .models import User
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email"]
