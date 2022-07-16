928b544e69057484f6bea281369354630add2029
Jaesik <joje1283@gmail.com>
2022-07-17 00:12:36 +0900

    물 주기 기능 개발

--- plants/models.py
+++ plants/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
+class WaterLog(models.Model):
+    plant = models.ForeignKey(to="Plant", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name="물 준 시간", auto_now_add=True)
+
+
 class Plant(models.Model):
     name = models.CharField(verbose_name="이름", max_length=50)
     species = models.CharField(verbose_name="종", max_length=50, default="")
     period = models.IntegerField(verbose_name="물주기 기간", default=7)
-    last_water_date = models.DateTimeField(verbose_name="마지막으로 물 준 시간", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def last_water_date(self):
+        return self.waterlog_set.order_by("-created_at").first()
+
+    def give_water(self):
+        WaterLog.objects.create(plant=self)
--- plants/tests.py
+++ plants/tests.py
@@ -1,14 +1,27 @@
 from django.test import TestCase
-from .models import Plant
+from .models import Plant, WaterLog
 
 
 class TestPlant(TestCase):
     def setUp(self) -> None:
-        pass
+        self.plant = Plant.objects.create(name="지키라", species="파키라")
 
-    def test_create_model(self):
-        plant = Plant.objects.create(name="지키라", species="파키라")
-        self.assertEqual(plant.name, "지키라")
-        self.assertEqual(plant.species, "파키라")
-        self.assertEqual(plant.period, 7)
-        self.assertIsNone(plant.last_water_date)
+    def test_model_data(self):
+        """
+        plant 테이블에 생성된 데이터를 확인
+        """
+        self.assertEqual(self.plant.name, "지키라")
+        self.assertEqual(self.plant.species, "파키라")
+        self.assertEqual(self.plant.period, 7)
+        self.assertIsNone(self.plant.last_water_date)
+
+    def test_give_water(self):
+        """
+        물 준 기록이 잘 생성되었는지 확인
+        """
+        self.assertEqual(WaterLog.objects.count(), 0)
+        self.plant.give_water()
+        self.assertEqual(WaterLog.objects.count(), 1)
+        water_log = self.plant.last_water_date
+        self.assertEqual(water_log.plant, self.plant)
+
