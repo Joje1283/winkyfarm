81365c383dd9515b767282cf8476f9f9788383e8
Jaesik <joje1283@gmail.com>
2022-07-25 01:25:06 +0900

    Feature/cicd적용 (#4)
    * * 앱, 워커를 위해 각각 dockerfile 작성
    * 배포환경에 맞게 static root 위치 지정
    * postgres는 배포환경에서만 사용하도록 수정
    * 배포를 위한 기타 라이브러리 추가 (gunicorn, uvicorn, celery)
    
    * branch 정보를 가져오기 위해 git pull 명령어 추가
    
    * collectstatic 수행 명령에 virtualenv 적용
    
    * CSRF 보안으로 인해 CSRF_TRUSTED_ORIGINS 추가
    
    * 쿠버네티스 설정 파일 백업

--- winkyfarm/settings/dev.py
+++ winkyfarm/settings/dev.py
@@ -5,3 +5,22 @@ MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware",]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+if 'test' in sys.argv:
+    DATABASES = {
+        "default":
+            {'ENGINE': 'django.db.backends.sqlite3'}
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'winkyfarm',
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
+    }
--- winkyfarm/settings/prod.py
+++ winkyfarm/settings/prod.py
@@ -1,1 +1,27 @@
 from .common import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS += [os.getenv("ALLOWED_HOST")]
+CSRF_TRUSTED_ORIGINS = ["https://*.joje.link"]  # 안전하지 않은 요청에 대한 신뢰할 수 있는 출처 목록 (CSRF 오류로 인해 설정)
+STATIC_ROOT = "/var/www/staticfiles/winkyfarm"
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+if 'test' in sys.argv:
+    DATABASES = {
+        "default":
+            {'ENGINE': 'django.db.backends.sqlite3'}
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'winkyfarm',
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
+    }
