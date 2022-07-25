a0e083483cdf3e997bb7e8a4cc265fd290ae66fd
Jaesik <joje1283@gmail.com>
2022-07-25 12:55:38 +0900

    sentry 적용

--- winkyfarm/urls.py
+++ winkyfarm/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
 
 if settings.DEBUG:
